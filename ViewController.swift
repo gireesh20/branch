18522fe12dcc84d602e6af5b0acbd2d73545c68f
gireesh20 <gireesh20.chanti@gmail.com>
2020-12-09 11:52:51 +0530

    Add files via upload
    New Project

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubBranch
+//
+//  Created by Idcadmin on 18/09/1942 Saka.
+//  Copyright © 1942 Idcadmin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
